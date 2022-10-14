273146e8fe738cdf27a6faf03cd7f0c028646803
Kevin Lee <37029620+kdog3682@users.noreply.github.com>
2022-10-14 02:57:12 -0400

    --

---
+++ math-doc-string.js
@@ -0,0 +1,13 @@
+const mathDocRef = {
+  "sudoku": {
+    "instructions": "Fill in the empty boxes with a number from 1 to 4.\nThe rule is no row or column can repeat numbers.",
+    "title": "Sudoku"
+  },
+  "warmup": {
+    "title": "Warmup"
+  },
+  "sequences": {
+    "title": "Number Patterns",
+    "instructions": "For each set of numbers, look for a pattern.\nThe pattern will help you figure out the missing numbers."
+  }
+}
